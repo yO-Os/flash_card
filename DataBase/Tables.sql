6842bfb5cc4f168b7d831912e430f3b3ca42a9ec
yO-Os <yzerufaeil@gmail.com>
2025-09-22 22:17:51 +0300

    Schema

---
+++ DataBase/Tables.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE `FLASHCARD`
+USE `FLASHCARD`
+CREATE TABLE `User`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Name` VARCHAR(100) NOT NULL unique,
+    `Email` VARCHAR(255) NOT NULL unique,
+    `Password_hash` VARCHAR(255) NOT NULL,
+    `Created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE `Deck`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `User_id` INT UNSIGNED NOT NULL,
+    `Title` VARCHAR(100) NOT NULL,
+    `Description` TEXT NOT NULL,
+    FOREIGN KEY (`User_id`) REFERENCES `User`(`id`)
+);
+CREATE TABLE `FlashCards`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Deck_id` INT UNSIGNED NOT NULL,
+    `Question` TEXT NOT NULL,
+    `Answer` TEXT NOT NULL,
+    FOREIGN KEY (`Deck_id`) REFERENCES `Deck`(`id`)
+);
+CREATE TABLE `Study_session`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `User_id` INT UNSIGNED NOT NULL,
+    `Deck_id` INT UNSIGNED NOT NULL,
+    `Studid_at` TIMESTAMP NOT NULL,
+    FOREIGN KEY (`User_id`) REFERENCES `User`(`id`),
+    FOREIGN KEY (`Deck_id`) REFERENCES `Deck`(`id`)
+);
+CREATE TABLE `Progress`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `User_id` INT UNSIGNED NOT NULL,
+    `Card_id` INT UNSIGNED NOT NULL,
+    `Attempts` INT NOT NULL,
+    `Correct` INT NOT NULL,
+    `Last_review` TIMESTAMP NOT NULL,
+    FOREIGN KEY (`User_id`) REFERENCES `User`(`id`),
+    FOREIGN KEY (`Card_id`) REFERENCES `FlashCards`(`id`)
+    );
